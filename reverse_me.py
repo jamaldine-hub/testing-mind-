eddcc6c8e5fb66846e32f6fa2ce439d0ac667740
zakaria jamaldine <jamaldinezakaria@gmail.com>
2025-10-02 03:33:56 +0100

    first-version

---
+++ reverse_me.py
@@ -0,0 +1,15 @@
+import pyttsx3
+import speech_recognition as sr
+# reverse me function
+def reverse_me():
+    engine = pyttsx3.init()
+    rec = sr.Recognizer()
+    with sr.Microphone() as src:
+        print("Say something ...")
+        audio = rec.recognize_google(src)
+        print(f"you said {audio}")
+    audio = audio[::-1]
+    print(f"your reverse {audio}")
+    engine.say(audio)
+    engine.runAndWait()
+reverse_me()
